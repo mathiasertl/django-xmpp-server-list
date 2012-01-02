b22bca8e1c8618704f9e0f45bb55422bf818d93e
Mathias Ertl <mati@fsinf.at>
2012-01-02 11:04:50 +0100

    script to add initial data

---
+++ init.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python
+
+import os, sys, datetime
+if 'DJANGO_SETTINGS_MODULE' not in os.environ:
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'xmpplist.settings'
+sys.path.append( os.getcwd() )
+sys.path.append( os.path.dirname(os.getcwd()) )
+
+from django.contrib.auth.models import User
+from account.models import UserProfile
+from server.models import Server, ServerSoftware, CertificateAuthority
+
+u = User.objects.create(username='mati')
+u.set_password('nopass')
+p = UserProfile.objects.create(user=u, email_confirmed=True)
+
+startssl = CertificateAuthority.objects.create(name='StartSSL', website='https://www.startssl.com')
+cacert = CertificateAuthority.objects.create(name='CAcert', website='https://www.cacert.org')
+
+ejabberd = ServerSoftware.objects.create(name='ejabberd', website='http://www.ejabberd.im',
+                                         newest_version='2.1.10')
+prosody = ServerSoftware.objects.create(name='prosody', website='http://www.prosody.im',
+                                         newest_version='0.8.2')
+
+Server.objects.create(user=u, ca=startssl, software=ejabberd,
+    domain='jabber.at', website='https://jabber.at',
+    longitude=16.367419, latitude=48.199936,
+    launched=datetime.date.today(), software_version='2.1.10',
+    contact='chat@conference.jabber.at', contact_name='chat@conference.jabber.at', contact_type='M'
+)
+Server.objects.create(user=u, ca=startssl, software=ejabberd,
+    domain='jabber.fsinf.at', website='https://jabber.fsinf.at',
+    longitude=16.367419, latitude=48.199936,
+    launched=datetime.date.today(), software_version='2.1.10',
+    contact='fsinf@conference.fsinf.at', contact_name='fsinf@conference.fsinf.at', contact_type='M'
+)
