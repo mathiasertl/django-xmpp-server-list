abea7fb98cd567f579ec0f41dfe7264f54adf752
Mathias Ertl <mati@fsinf.at>
2012-01-02 09:30:29 +0100

    filter public servers on correct properties

--- views.py
+++ views.py
@@ -3,5 +3,7 @@ from server.models import Server
 from django.shortcuts import render
 
 def home(request):
-    servers = Server.objects.filter(checked__isnull=False, srv_ok=True)
+    servers = Server.objects.filter(
+        verified=True, moderated=True, user__profile__email_confirmed=True)
+
     return render(request, 'index.html', {'servers': servers})
