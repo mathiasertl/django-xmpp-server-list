9ccac04b7aa500ef4109e48c617ef8cb8a3715b7
Mathias Ertl <mati@fsinf.at>
2012-01-03 11:41:02 +0100

    simplify template imensly

---
+++ server/templatetags/fieldvalue.py
@@ -0,0 +1,16 @@
+from django import template
+from django.forms.fields import DateField
+
+register = template.Library()
+
+@register.filter
+def fieldvalue(field):
+    if hasattr(field.field, 'choices'):
+        for k, v in field.field.choices:
+            if k == field.value():
+                return v
+        return 'key not found'
+    elif type(field.field) == DateField:
+        return field.value().strftime('%Y-%m-%d')
+    else:
+        return field.value()
