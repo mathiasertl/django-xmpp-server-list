2f89375e2655e743f0abb38b2afd710110858847
Mathias Ertl <mati@fsinf.at>
2012-01-06 22:05:25 +0100

    add api help

--- api/urls.py
+++ api/urls.py
@@ -3,4 +3,5 @@ from django.conf.urls.defaults import *
 urlpatterns = patterns(
     'api.views',
     url(r'^$', 'index', name='api'),
+    url(r'^help/$', 'help', name='api_help'),
 )
--- api/views.py
+++ api/views.py
@@ -1,7 +1,8 @@
 import json
 
-from django.http import HttpResponse
 from django.core import serializers
+from django.http import HttpResponse, HttpResponseForbidden
+from django.shortcuts import render
 
 from xmpplist.server.models import Server
 from xmpplist.world.models import WorldBorders
@@ -12,16 +13,16 @@ def index(request):
         verified=True, moderated=True, user__profile__email_confirmed=True)
     
     # filter by required features:
-    if 'require' in request.GET:
-        features = request.GET['require'].split(',')
+    if 'features' in request.GET:
+        features = request.GET['features'].split(',')
         if 'plain' in features:
-            servers = servers.filter(support_plain=True)
+            servers = servers.filter(features__has_plain=True)
         if 'ssl' in features:
-            servers = servers.filter(support_ssl=True)
+            servers = servers.filter(features__has_ssl=True)
         if 'tls' in features:
-            servers = servers.filter(support_tls=True)
+            servers = servers.filter(features__has_tls=True)
         if 'ipv6' in features:
-            servers = servers.filter(features__ipv6=True)
+            servers = servers.filter(features__has_ipv6=True)
     
     # filter by country
     if 'country' in request.GET:
@@ -30,20 +31,21 @@ def index(request):
     
     fields = ['domain']
     if 'fields' in request.GET:
-        custom_fields = set(request.GET['fields'].split())
-        valid_fields = set(['launched', 'location', 'website', 'ca', 'software',
-                            'software_version', 'contact'])
-        if custom_fields - valid_fields:
+        custom_fields = request.GET['fields'].split(',')
+        print(custom_fields)
+        valid_fields = ['launched', 'location', 'website', 'ca', 'software',
+                        'software_version', 'contact']
+        if set(custom_fields) - set(valid_fields):
             return HttpResponseForbidden("tried to retrieve forbidden fields.")
             
         if 'ca' in custom_fields:
-            custom_fields.pop('ca')
+            custom_fields.remove('ca')
             custom_fields.append('ca__name')
         if 'software' in custom_fields:
-            custom_fields.pop('software')
+            custom_fields.remove('software')
             custom_fields.append('software__name')
         if 'contact' in custom_fields:
-            custom_fields.pop('contact')
+            custom_fields.remove('contact')
             custom_fields += ['contact', 'contact_type']
             
         fields += custom_fields
@@ -51,6 +53,31 @@ def index(request):
     if len(fields) == 1:
         values = list(servers.values_list(*fields, flat=True))
     else:
-        values = list(servers.values(*fields))
+        tmp_values = list(servers.values(*fields))
+        values = {}
+        for value in tmp_values:
+            domain = value.pop('domain')
+            
+            if 'ca__name' in value:
+                ca = value.pop('ca__name')
+                value['ca'] = ca
+            if 'software__name' in value:
+                software = value.pop('software__name')
+                value['software'] = software
+            if 'contact' in value:
+                contact = value.pop('contact')
+                contact_type = value.pop('contact_type')
+                value['contact'] = (contact, contact_type)
+            if 'location' in value:
+                location = value.pop('location')
+                value['location'] = '%s,%s' % (location.x, location.y)
+            if 'launched' in value:
+                launched = value.pop('launched')
+                value['launched'] = launched.strftime('%Y-%m-%d')
+                
+            values[domain] = value
     
     return HttpResponse(json.dumps(values))
+    
+def help(request):
+    return render(request, 'api/help.html')
