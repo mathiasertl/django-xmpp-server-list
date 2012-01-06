d4eed73012c49e4398492cbd0b14ee9965aef59a
Mathias Ertl <mati@fsinf.at>
2012-01-06 22:04:31 +0100

    add features to admin

--- server/admin.py
+++ server/admin.py
@@ -1,9 +1,10 @@
 #from django.contrib import admin
 from django.contrib.gis import admin
 
-from models import ServerReport, Server, CertificateAuthority, ServerSoftware
+from models import ServerReport, Server, CertificateAuthority, ServerSoftware, Features
 
 admin.site.register(Server, admin.OSMGeoAdmin)
 admin.site.register(ServerSoftware)
 admin.site.register(ServerReport)
+admin.site.register(Features)
 admin.site.register(CertificateAuthority)
