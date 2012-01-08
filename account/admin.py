af6b5666bd8d4b580eb1da8909d8b39f9db54442
Mathias Ertl <mati@fsinf.at>
2012-01-08 13:37:57 +0100

    remove ConfirmationKey (now in seperate app)

--- account/admin.py
+++ account/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 
-from models import ConfirmationKey, UserProfile
+from models import UserProfile
 
 admin.site.register(UserProfile)
-admin.site.register(ConfirmationKey)
