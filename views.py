33df445c914e3beb96a9304516d8c0d14069e6c7
Mathias Ertl <mati@fsinf.at>
2012-01-11 12:41:42 +0100

    also require JID to be confirmed

--- views.py
+++ views.py
@@ -3,7 +3,7 @@ from server.models import Server
 from django.shortcuts import render
 
 def home(request):
-    servers = Server.objects.filter(
-        verified=True, moderated=True, user__profile__email_confirmed=True)
+    servers = Server.objects.filter(verified=True, moderated=True,
+        user__profile__email_confirmed=True, user__profile__jid_confirmed=True)
 
     return render(request, 'index.html', {'servers': servers})
