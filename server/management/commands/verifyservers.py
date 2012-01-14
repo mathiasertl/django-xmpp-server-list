518412f724e6684427dd772b4503302034bc61f1
Mathias Ertl <mati@fsinf.at>
2012-01-14 13:16:26 +0100

    move to verifyservers command

---
+++ server/management/commands/verifyservers.py
@@ -0,0 +1,10 @@
+from django.core.management.base import BaseCommand, CommandError
+from xmpplist.server.models import Server
+
+class Command(BaseCommand):
+#    args = '<poll_id poll_id ...>'
+    help = 'Verify servers'
+
+    def handle(self, *args, **options):
+        for server in Server.objects.all():
+            server.verify()
