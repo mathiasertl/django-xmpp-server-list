d737462519674eab0cd2bfd90ae6c869231d64a7
Mathias Ertl <mati@fsinf.at>
2013-03-24 19:29:36 +0100

    add moderationmails manage.py command

---
+++ server/management/commands/moderationmails.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.contrib.auth.models import User, Permission
+from django.core.mail import send_mail
+from django.core.management.base import BaseCommand, CommandError
+from django.db.models import Q
+from django.template import loader, Context
+
+from xmpplist.server.models import Server
+from xmpplist.server.util import get_siteinfo
+
+
+class Command(BaseCommand):
+    args = '<poll_id poll_id ...>'
+    help = 'Closes the specified poll for voting'
+
+    def handle(self, *args, **options):
+        perm = Permission.objects.get(codename='moderate')
+        query = Q(groups__permissions=perm) | Q(user_permissions=perm)
+        users = User.objects.filter(query | Q(is_superuser=True)).distinct()
+        protocol, domain = get_siteinfo()
+
+        servers = Server.objects.filter(moderated=None)
+        if not servers:
+            return
+
+        subject = '[%s] %s servers awaiting moderation' % (
+            settings.SITENAME, len(servers)
+        )
+
+        t = loader.get_template('mail/moderationmail.html')
+        for user in users:
+            c = Context({
+                'servers': servers,
+                'user': user,
+                'sitename': settings.SITENAME,
+                'protocol': protocol,
+                'domain': domain,
+            })
+
+            body = t.render(c)
+
+            send_mail(subject, body, settings.DEFAULT_FROM_EMAIL,
+                          [user.email], fail_silently=False)
