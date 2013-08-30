9713f046587e8ca26ce5c4904757a727edcd680e
Mathias Ertl <mati@fsinf.at>
2013-08-30 09:03:02 +0200

    minor fixes

--- api/views.py
+++ api/views.py
@@ -7,18 +7,17 @@ from django.http import HttpResponse, HttpResponseForbidden, HttpResponseBadRequ
 from django.shortcuts import render
 
 from xmpplist.server.models import Server
-from xmpplist.world.models import WorldBorders
 
 def index(request):
     # get request format
     request_format = 'json'
     if 'format' in request.GET:
         request_format = request.GET['format']
-    
+
     # initial query-set:
     servers = Server.objects.filter(
         verified=True, moderated=True, user__profile__email_confirmed=True)
-    
+
     # filter by required features:
     if 'features' in request.GET:
         features = request.GET['features'].split(',')
@@ -30,47 +29,48 @@ def index(request):
             servers = servers.filter(features__has_tls=True)
         if 'ipv6' in features:
             servers = servers.filter(features__has_ipv6=True)
-    
+
     # filter by country
-    if 'country' in request.GET:
-        country = WorldBorders.objects.get(iso2__iexact=request.GET['country'])
-        servers = servers.filter(location__within=country.geom)
-    
+#TODO: re-enable country filtering functionality? Was removed during location.
+#    if 'country' in request.GET:
+#        country = request.GET['country']
+#        servers = servers.filter(location__within=country.geom)
+
     fields = ['domain']
-    
+
     # http://xmpp.org/services/services.xml / http://xmpp.org/services/services-full.xml formats
     # already deprecated, i hope :)
     # does fetching + serialization in one step
     if request_format == 'services.xml' or request_format == 'services-full.xml':
         root_element = etree.Element('query') # , nsmap={None: 'http://jabber.org/protocol/disco#items'})
-        
+
         if request_format == 'services-full.xml':
             fields += ['software__name', 'contact', 'contact_type', 'website', 'location']
-        
+
         contact_prefixes = {'M': 'xmpp:', 'J': 'xmpp:', 'E': 'mailto:'}
-        
+
         values = list(servers.values(*fields))
-        
+
         for item in values:
             item_element = etree.SubElement(root_element, 'item')
             item_element.set('jid', item['domain'])
-                
+
             if request_format == 'services-full.xml':
                 etree.SubElement(item_element, 'server-software').text = item['software__name']
                 etree.SubElement(item_element, 'domain').text = item['domain']
                 etree.SubElement(item_element, 'homepage').text = item['website']
-                
+
                 contact_prefix = ''
                 if item['contact_type'] in contact_prefixes:
                     contact_prefix = contact_prefixes[item['contact_type']]
-                
+
                 etree.SubElement(item_element, 'primary-admin').text = contact_prefix + item['contact']
                 etree.SubElement(item_element, 'longitude').text = str(item['location'].x)
                 etree.SubElement(item_element, 'latitude').text = str(item['location'].y)
                 etree.SubElement(item_element, 'description').text = None
-            
+
         return HttpResponse(etree.tostring(root_element, pretty_print=True), mimetype='text/xml')
-    
+
     # we now continue by parsing the fields parameter
     if 'fields' in request.GET:
         custom_fields = request.GET['fields'].split(',')
@@ -78,7 +78,7 @@ def index(request):
                         'software_version', 'contact']
         if set(custom_fields) - set(valid_fields):
             return HttpResponseForbidden("tried to retrieve forbidden fields.")
-            
+
         if 'ca' in custom_fields:
             custom_fields.remove('ca')
             custom_fields.append('ca__name')
@@ -88,9 +88,9 @@ def index(request):
         if 'contact' in custom_fields:
             custom_fields.remove('contact')
             custom_fields += ['contact', 'contact_type']
-            
+
         fields += custom_fields
-    
+
     if len(fields) == 1:
         values = list(servers.values_list(*fields, flat=True))
     else:
@@ -98,7 +98,7 @@ def index(request):
         values = {}
         for value in tmp_values:
             domain = value.pop('domain')
-            
+
             if 'ca__name' in value:
                 ca = value.pop('ca__name')
                 value['ca'] = ca
@@ -115,14 +115,14 @@ def index(request):
             if 'launched' in value:
                 launched = value.pop('launched')
                 value['launched'] = launched.strftime('%Y-%m-%d')
-                
+
             values[domain] = value
 
     if request_format == 'json':
         return HttpResponse(json.dumps(values))
     else:
         return HttpResponseBadRequest('unknown request format: try "services.xml", "services-full.xml" or "json"')
-    
-    
+
+
 def help(request):
     return render(request, 'api/help.html')
