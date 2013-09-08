5def9495dc8377b17e5117d6ca8508f57b523c9f
Mathias Ertl <mati@fsinf.at>
2013-09-08 10:54:45 +0200

    delete user profile

--- account/models.py
+++ account/models.py
@@ -16,22 +16,7 @@
 # along with xmpplist.  If not, see <http://www.gnu.org/licenses/>.
 
 from django.db import models
-from django.contrib.auth.models import User
 from django.contrib.auth.models import AbstractUser
-
-
-class UserProfile(models.Model):
-    user = models.OneToOneField(User, related_name='profile')
-    email_confirmed = models.BooleanField(default=False)
-
-    jid = models.CharField(
-        max_length=128,
-        help_text="Required, a confirmation message will be sent to this "
-        "address.")
-    jid_confirmed = models.BooleanField(default=False)
-
-    def __unicode__(self):
-        return self.user.username
 
 
 class LocalUser(AbstractUser):
