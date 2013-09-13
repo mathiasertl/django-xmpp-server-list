f8ade356d0986b46bd5df63b151c15bbe876f2fa
Mathias Ertl <mati@fsinf.at>
2013-09-13 11:21:30 +0200

    cleanup plugin

--- xmpp/plugins/amp.py
+++ xmpp/plugins/amp.py
@@ -1,7 +1,25 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of xmpplist (https://list.jabber.at).
+#
+# xmpplist is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# xmppllist is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with xmpplist.  If not, see <http://www.gnu.org/licenses/>.
+
 from sleekxmpp.plugins import BasePlugin
 from sleekxmpp.stanza import StreamFeatures
 from sleekxmpp.xmlstream import register_stanza_plugin
 from sleekxmpp.xmlstream import ElementBase
+
 
 class AmpStanza(ElementBase):
     name = 'amp'
@@ -17,10 +35,11 @@ class feature_amp(BasePlugin):
     """
 
     def plugin_init(self):
-        self.xmpp.register_feature('amp',
-                self._handle_amp,
-                restart=False,
-                order=self.config.get('order', 0))
+        self.xmpp.register_feature(
+            'amp',
+            self._handle_amp,
+            restart=False,
+            order=self.config.get('order', 0))
 
         register_stanza_plugin(StreamFeatures, AmpStanza)
 
