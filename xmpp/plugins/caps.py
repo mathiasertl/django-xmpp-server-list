4d1173453577212e4aa8c0dd49c73a74163802a8
Mathias Ertl <mati@fsinf.at>
2013-09-13 11:43:43 +0200

    polish various plugins

--- xmpp/plugins/caps.py
+++ xmpp/plugins/caps.py
@@ -1,7 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of xmpplist (https://list.jabber.at).
+#
+# xmpplist is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# xmppllist is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with xmpplist.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
 from sleekxmpp.plugins import BasePlugin
 from sleekxmpp.stanza import StreamFeatures
 from sleekxmpp.xmlstream import register_stanza_plugin
 from sleekxmpp.xmlstream import ElementBase
+
 
 class CapsStanza(ElementBase):
     name = 'c'
@@ -14,10 +34,11 @@ class feature_caps(BasePlugin):
     """Caps"""
 
     def plugin_init(self):
-        self.xmpp.register_feature('c',
-                self._handle_caps,
-                restart=False,
-                order=self.config.get('order', 0))
+        self.xmpp.register_feature(
+            'c',
+            self._handle_caps,
+            restart=False,
+            order=self.config.get('order', 0))
 
         register_stanza_plugin(StreamFeatures, CapsStanza)
 
--- xmpp/plugins/compression.py
+++ xmpp/plugins/compression.py
@@ -1,7 +1,27 @@
+# -*- coding: utf-8 -*-
+#
+# This file is part of xmpplist (https://list.jabber.at).
+#
+# xmpplist is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# xmppllist is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with xmpplist.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import unicode_literals
+
 from sleekxmpp.plugins import BasePlugin
 from sleekxmpp.stanza import StreamFeatures
 from sleekxmpp.xmlstream import register_stanza_plugin
-from sleekxmpp.xmlstream import StanzaBase, ElementBase
+from sleekxmpp.xmlstream import ElementBase
+
 
 class CompressionStanza(ElementBase):
     name = 'compression'
@@ -15,10 +35,11 @@ class feature_compression(BasePlugin):
 
     def plugin_init(self):
         self.description = "Compression"
-        self.xmpp.register_feature('compression',
-                self._handle_compression,
-                restart=False,
-                order=self.config.get('order', 0))
+        self.xmpp.register_feature(
+            'compression',
+            self._handle_compression,
+            restart=False,
+            order=self.config.get('order', 0))
 
         register_stanza_plugin(StreamFeatures, CompressionStanza)
 
