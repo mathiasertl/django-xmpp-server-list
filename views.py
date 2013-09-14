768583001f61b5fb81acd5b7cc2c925e209e49a9
Mathias Ertl <mati@fsinf.at>
2013-09-14 18:27:38 +0200

    update views

--- views.py
+++ views.py
@@ -19,7 +19,7 @@ from server.models import Server
 from django.shortcuts import render
 
 def home(request):
-    servers = Server.objects.filter(verified=True, moderated=True,
-        user__email_confirmed=True, user__jid_confirmed=True).verified().order_by('domain')
+    servers = Server.objects.filter(
+        moderated=True, user__email_confirmed=True, user__jid_confirmed=True).verified().order_by('domain')
 
     return render(request, 'index.html', {'servers': servers})
