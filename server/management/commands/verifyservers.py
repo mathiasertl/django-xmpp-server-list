4c05a4c3e794789b7f346ec63694ed764478b8bc
Mathias Ertl <mati@er.tl>
2014-08-30 15:33:15 +0200

    coding style, use logging correctly

--- server/management/commands/verifyservers.py
+++ server/management/commands/verifyservers.py
@@ -3,18 +3,16 @@
 # This file is part of django-xmpp-server-list
 # (https://github.com/mathiasertl/django-xmpp-server-list).
 #
-# django-xmpp-server-list is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
+# django-xmpp-server-list is free software: you can redistribute it and/or modify it under the
+# terms of the GNU General Public License as published by the Free Software Foundation, either
+# version 3 of the License, or (at your option) any later version.
 #
-# xmppllist is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
+# django-xmpp-server-list is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE.  See the GNU General Public License for more details.
 #
-# You should have received a copy of the GNU General Public License
-# along with django-xmpp-server-list.  If not, see <http://www.gnu.org/licenses/>.
+# You should have received a copy of the GNU General Public License along with
+# django-xmpp-server-list.  If not, see <http://www.gnu.org/licenses/>.
 
 import logging
 import os
@@ -26,6 +24,8 @@ from datetime import timedelta
 from django.core.management.base import BaseCommand
 from server.models import Server
 
+log = logging.getLogger(__name__)
+
 
 class Command(BaseCommand):
     args = '[domain ...]'
@@ -35,17 +35,18 @@ class Command(BaseCommand):
         # some initial cleanup first:
         delta = datetime.today() - timedelta(days=14)
         check_delta = datetime.today() - timedelta(days=2)
-        Server.objects.filter(
-            last_seen__lt=delta, last_checked__gt=check_delta).delete()
+        Server.objects.filter(last_seen__lt=delta, last_checked__gt=check_delta).delete()
 
         if args:
             for domain in args:
                 try:
+                    log.info('Verifying %s', domain)
                     Server.objects.get(domain=domain).verify()
                 except Server.DoesNotExist:
-                    logging.error('Could not find %s', domain)
+                    log.error('Could not find %s', domain)
         else:
             for server in Server.objects.all().order_by('domain'):
+                log.info('Verifying %s', server.domain)
                 server.verify()
 
         time.sleep(2)
