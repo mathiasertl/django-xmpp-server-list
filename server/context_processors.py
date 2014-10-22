bb6afdcf61bd7fbb248f0486bffd5ca8ba0a626e
Mathias Ertl <mati@er.tl>
2014-10-22 10:45:02 +0200

    pass request to get_siteinfo

--- server/context_processors.py
+++ server/context_processors.py
@@ -20,9 +20,7 @@ from server.util import get_siteinfo
 
 
 def siteinfo(request):
-    protocol, domain = get_siteinfo()
-    if request.is_secure():
-        protocol = 'https'
+    protocol, domain = get_siteinfo(request)
     return {
         'domain': domain,
         'protocol': protocol,
