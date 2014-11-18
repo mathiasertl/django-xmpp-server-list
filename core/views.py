8614e23c85217e2d42c29d928fdceb90208f2ac0
Mathias Ertl <mati@fsinf.at>
2014-11-18 13:00:21 +0100

    add a LoginRequiredMixin

--- core/views.py
+++ core/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+class LoginRequiredMixin(object):
+    @classmethod
+    def as_view(cls, **initkwargs):
+        view = super(LoginRequiredMixin, cls).as_view(**initkwargs)
+        return login_required(view)
