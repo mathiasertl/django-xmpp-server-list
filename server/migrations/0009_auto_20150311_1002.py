3cd6f54566612610fcb1f5d03a1721d67996d940
Mathias Ertl <mati@er.tl>
2015-03-11 16:03:19 +0100

    add some migration that Django seems to think I should do

---
+++ server/migrations/0009_auto_20150311_1002.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('server', '0008_server_moderation_message'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='server',
+            name='website',
+            field=models.URLField(help_text=b'A homepage where one can find information on your server. If left empty, the default is https://<domain>.', blank=True),
+            preserve_default=True,
+        ),
+    ]
