766c163a1142a6c159b2c63690ec9aa863070c72
Mathias Ertl <mati@er.tl>
2019-02-08 18:39:44 +0100

    largely fix templates

--- account/urls.py
+++ account/urls.py
@@ -14,30 +14,30 @@
 # You should have received a copy of the GNU General Public License
 # along with django-xmpp-server-list.  If not, see <http://www.gnu.org/licenses/>.
 
-from django.conf.urls import url
+from django.urls import path
 from django.contrib.auth import views as auth_views
 
 from . import views
 from .forms import AuthenticationFormSub
 from .forms import SetPasswordForm
 
-# TODO: use path instead of url
-# TODO: use namespace instead of account_ prefix in name
+app_name = 'account'
+
 urlpatterns = [
-    url(r'^$', views.index, name='account'),
-    url(r'^create/$', views.create, name='account_create'),
-    url(r'^edit/$', views.edit, name='account_edit'),
-    url(r'^reset_password/$', views.ResetPassword.as_view(), name='account_reset_password'),
-    url(r'^reset_password/done/$', views.reset_password_ok, name='account_reset_password_ok'),
-    url(r'^resend_confirmation/$', views.resend_confirmation, name='account_resend_confirmation'),
-    url(r'^login/', auth_views.LoginView.as_view(
+    path('', views.index, name='index'),
+    path('create/', views.create, name='create'),
+    path('edit/', views.edit, name='edit'),
+    path('reset_password/', views.ResetPassword.as_view(), name='reset_password'),
+    path('reset_password/done/', views.reset_password_ok, name='reset_password_ok'),
+    path('resend_confirmation/', views.resend_confirmation, name='resend_confirmation'),
+    path('login/', auth_views.LoginView.as_view(
         template_name='account/login.html',  # TODO: use the default
         authentication_form=AuthenticationFormSub,  # TODO: necessary?
     ), name='login'),
-    url(r'^logout/', auth_views.LogoutView.as_view(template_name='logout.html')),  # TODO: use default
-    url(r'^password/', auth_views.PasswordChangeView.as_view(
+    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html')),  # TODO: use default
+    path('password/', auth_views.PasswordChangeView.as_view(
         template_name='account/set_password.html',  # TODO: use default
         #post_change_redirect='/user',  # TODO?
         form_class=SetPasswordForm  # TODO: use default?
-    ), name='account_set_password')
+    ), name='set_password')
 ]
