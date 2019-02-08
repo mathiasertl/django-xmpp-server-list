00662ce1c4f919cd7b6c97d22385281f0359cbac
Mathias Ertl <mati@er.tl>
2019-02-08 19:06:37 +0100

    use default template locations

--- account/urls.py
+++ account/urls.py
@@ -31,12 +31,10 @@ urlpatterns = [
     path('reset_password/done/', views.reset_password_ok, name='reset_password_ok'),
     path('resend_confirmation/', views.resend_confirmation, name='resend_confirmation'),
     path('login/', auth_views.LoginView.as_view(
-        template_name='account/login.html',  # TODO: use the default
         authentication_form=AuthenticationFormSub,  # TODO: necessary?
     ), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='logout.html')),  # TODO: use default
+    path('logout/', auth_views.LogoutView.as_view()),
     path('password/', auth_views.PasswordChangeView.as_view(
-        template_name='account/set_password.html',  # TODO: use default
         #post_change_redirect='/user',  # TODO?
         form_class=SetPasswordForm  # TODO: use default?
     ), name='set_password')
