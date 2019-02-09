c25b69603283fa8580e36b0c7acb19a8bbdb6f28
Mathias Ertl <mati@er.tl>
2019-02-09 09:55:52 +0100

    remove (non-working) two-click like button

--- core/static/js/base.js
+++ core/static/js/base.js
@@ -33,10 +33,6 @@ $(document).ready(function() {
     register_tooltips();
     register_popover();
 
-    $('.fblike').on('click', function(event) {
-        fblike_button();
-    });
-
     $(".button-ok").on("click", function() {
         url = $(this).parent().attr('data-url');
         row = $(this).parent().parent();
@@ -70,22 +66,3 @@ $(document).ready(function() {
         });
     });
 });
-
-// Generate a two-click like button.
-// Shamelessly stolen from here: http://turkeyland.net/projects/two-click/
-var fblike_button = function() {
-    // Generate a string containing the HTML to place in the element (for readability)
-    var html = "<div id=\"fb-root\">\n";
-    html += "<div class=\"fb-like\" data-href=\"https://facebook.com/jabber.at\" data-send=\"true\" data-layout=\"button_count\" data-width=\"100\" data-show-faces=\"false\" data-font=\"arial\">\n";
-    html += "</div>\n";
-
-    // Replace the specified element's contents with the HTML necessary to display the
-    // Like/+1 Buttons, *before* loading the SDKs below
-    $('.fblike').html(html);
-
-    // This is the code provided by facebook to asynchronously load their SDK
-    var e = document.createElement('script'); e.async = true;
-    e.src = document.location.protocol +
-      '//connect.facebook.net/en_US/all.js#xfbml=1';
-    document.getElementById('fb-root').appendChild(e);
-}
