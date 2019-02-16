522c169672d4ee5b49dfb70fd40615ac8741027a
Mathias Ertl <mati@er.tl>
2019-02-16 10:50:06 +0100

    add BaseModel

--- core/models.py
+++ core/models.py
@@ -1,3 +1,25 @@
-#from django.db import models
+# This file is part of django-xmpp-server-list
+# (https://github.com/mathiasertl/django-xmpp-server-list).
+#
+# django-xmpp-server-list is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# xmppllist is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with django-xmpp-server-list.  If not, see <http://www.gnu.org/licenses/>
 
-# Create your models here.
+from django.db import models
+
+
+class BaseModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
