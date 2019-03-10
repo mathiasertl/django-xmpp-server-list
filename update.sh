ba42e41aa8d1f39869c42b0b69e274c30dcfc5e3
Mathias Ertl <mati@er.tl>
2019-03-10 14:25:36 +0100

    install deps

--- update.sh
+++ update.sh
@@ -12,6 +12,18 @@ if [[ -e update.rc ]]; then
     . update.rc
 fi
 
+# Don't install necessary dependencies, seems unsafe for the general usecase
+APT_DEPENDENCIES=zlib1g-dev
+
+if [[ "${INSTALL_APT_DEPENDENCIES}" == "y" ]]; then
+    apt-get update
+    apt-get install -y ${APT_DEPENDENCIES}
+
+    if [[ -z "${EXTRA_APT_DEPENDENCIES}" ]]; then
+        apt-get install -y ${EXTRA_APT_DEPENDENCIES}
+    fi
+fi
+
 pip install -U -r requirements.txt ${EXTRA_PIP_REQUIREMENTS}
 python manage.py migrate
 python manage.py collectstatic --noinput
