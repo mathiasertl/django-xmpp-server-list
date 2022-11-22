10fd2de2dde8b9c28b2510e82c1c7aa1d9968129
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-11-22 01:22:06 +0000

    Adding tarfile member sanitization to extractall()

--- core/utils.py
+++ core/utils.py
@@ -79,4 +79,23 @@ def refresh_geoip_database():
                 member.name = os.path.basename(member.name)
             log.info('Extracting %s to %s', ', '.join([m.name for m in members]), settings.GEOIP_CONFIG_ROOT)
 
-            tar.extractall(settings.GEOIP_CONFIG_ROOT, members)
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar, settings.GEOIP_CONFIG_ROOT, members)
